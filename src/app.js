a5ef90f4f555598add6473a57742b1ccbabbdcc4
trayan-iliev <trayan.iliev@gmail.com>
2023-08-25 07:56:27 +0300

    Added CSS reset & some global styles

---
+++ src/app.js
@@ -0,0 +1,19 @@
+var pointsLawful = 0;
+var pointsGood = 0;
+
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      points: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      }
+    },
+  ];
+  
