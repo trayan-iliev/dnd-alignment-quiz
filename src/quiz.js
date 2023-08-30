5c029466c5d94a2b42d9770db6442784f4ba6f77
trayan-iliev <trayan.iliev@gmail.com>
2023-08-30 15:19:53 +0300

    Update

--- src/quiz.js
+++ src/quiz.js
@@ -2,6 +2,9 @@
 var pointsLawful = 0;
 var pointsGood = 0;
 
+// FOR TESTING PURPOSES: Show score while doing the quiz
+document.getElementById("points").append(document.createTextNode(`Lawful pts: ${pointsLawful} ||| Good pts: ${pointsGood}`))
+
 // Questions
 var myQuestions = [
     {
@@ -36,41 +39,49 @@ var myQuestions = [
 function loadQuestion (userChoice) {
     // Get divs & clear them   
     const divQuestion = document.getElementById('question');
-    const divAnswers = document.getElementById('answers');
+    const formAnswers = document.getElementById('answers');
     while (divQuestion.firstChild) {divQuestion.removeChild(divQuestion.firstChild)};
-    while (divAnswers.firstChild) {divAnswers.removeChild(divAnswers.firstChild)};
+    while (formAnswers.firstChild) {formAnswers.removeChild(formAnswers.firstChild)};
 
     // Get question, answers & points
-    let currentQuesion = myQuestions.shift();
-    let currentQuestionText = Object.values(currentQuesion)[0];
-    let currentQuestionAnswers = Object.values(currentQuesion)[1];
-    let currentQuestionPoints = Object.values(currentQuesion)[2];
-
+    let currentQuestion = myQuestions.shift();
+    if (typeof currentQuestion === "undefined") {alert("FINISHED"); return;};   // Check if questions are over & go to results function
+    let currentQuestionText = Object.values(currentQuestion)[0];
+    let currentQuestionAnswers = Object.values(currentQuestion)[1];
+    // let currentQuestionPoints = Object.values(currentQuestion)[2];
+    
     // Populate question
     divQuestion.appendChild(document.createTextNode(currentQuestionText))
 
     // Populate answers
     const answerCount = Object.keys(currentQuestionAnswers).length;
     for (let i = 0; i < answerCount; i++) {
+        
+        // Get current answer, it's value and alignment switch
         let currentAnswer = Object.values(currentQuestionAnswers)[i];
-        let div = document.createElement('div');
-        div.className = 'answer';
-        div.innerText = currentAnswer  ;
-        divAnswers.appendChild(div);
+        // let currentPoints = document.createTextNode(Object.values(currentQuestionPoints)[i]);
+        // currentPoints = currentPoints.split(' ');
+        // currentAlignment = currentPoints[1];
+        // currentPoints = currentPoints[0];
+        // currentPoints = parseInt(currentPoints);
+
+        let radioButtonDiv = document.createElement('div');
+        radioButtonDiv.className = 'answer';
+        let radioButton = document.createElement('input');
+        radioButton.type='radio';
+        radioButton.name = 'answer';
+        let radioButtonLabel = document.createElement('label');
+        radioButtonLabel.textContent = currentAnswer;
+        radioButtonDiv.appendChild(radioButton);
+        radioButtonDiv.appendChild(radioButtonLabel);
+        formAnswers.appendChild(document.createElement('br'));
     }
 
 
 
-
-    // Console log tests
-    console.log(currentQuestionText);
-    console.log(currentQuestionAnswers);
-    console.log(Object.values(currentQuestionAnswers).length);
-    console.log(currentQuestionPoints);
-    console.log(myQuestions);
-
 }
 
 
-
-// loadQuestion();
+function answerQuestion(answer) {
+    alert('submitted');
+}
